0bd7c8511331dec01b6027c9d54b37a3f81e0051
Artem Kholodenko <artemk@gmail.com>
2013-09-09 22:39:27 -0700

    refactored HOLD button code; added NEXT button

--- views/view.suggestion.js
+++ views/view.suggestion.js
@@ -4,6 +4,7 @@ var init_suggestion_view = function () {
 	App.SuggestionView = Backbone.View.extend({
 		el: $(".app"),
 		hold_button: null,
+		next_button: null,
 		is_hold: false,
 		template: _.template(App.tpl.get('suggestion.view')),
 		initialize: function () {
@@ -24,6 +25,7 @@ var init_suggestion_view = function () {
 		renderSuggestion: function () {
 			this.$el.html(this.template(this.model.toJSON()));	// add template to view in UI
 			this.init_hold_button();
+			this.init_next_button();
 			return this;
 		},
 		// render UI of place "held" by user
@@ -36,25 +38,37 @@ var init_suggestion_view = function () {
 		init_hold_button: function () {
 			var button_state = (this.is_hold) ? 'on' : 'off';	// determine if button is rendered turned ON or OFF
 
-			this.$el.append("<div id='hold_button' class='round_corners_default " + button_state + "'>HOLD</div>");
+			this.$el.append("<div id='hold_button' class='button round_corners_default " + button_state + "'>HOLD</div>");
 			this.hold_button = $("#hold_button");
 		},
+		// initialize next button in UI
+		init_next_button: function () {
+			this.$el.append("<div id='next_button' class='button round_corners_default'>NEXT</div>");
+			this.next_button = $('#next_button');
+		},
 		events : {
-			"click #hold_button" : "hold_button_toggle"
+			"click #hold_button" : "hold_button_toggle",
+			"click #next_button" : "next_button_click"
 		},
 		hold_button_toggle: function (e) {
 			// check if button is ON or OFF
-			if(this.hold_button.hasClass('off')) {
-				this.hold_button.removeClass('off').addClass('on');	// update UI for holding
-				this.is_hold = true;	// set flag on for holding
-				Utils.set_hold(this.model.toJSON());	// store place data in local storage
-			}
-			else {
-				this.hold_button.removeClass('on').addClass('off');	// update UI for not holding
-				this.is_hold = false;	// set flag off for holding
-				Utils.clear_hold();	// clear stored value in local storage
-			}
+			if(this.hold_button.hasClass('off')) this.hold_on();
+			else  this.hold_off();
 
+		},
+		hold_on: function () {
+			this.hold_button.removeClass('off').addClass('on');	// update UI for holding
+			this.is_hold = true;	// set flag on for holding
+			Utils.set_hold(this.model.toJSON());	// store place data in local storage
+		},
+		hold_off: function () {
+			this.hold_button.removeClass('on').addClass('off');	// update UI for not holding
+			this.is_hold = false;	// set flag off for holding
+			Utils.clear_hold();	// clear stored value in local storage
+		},
+		next_button_click: function (e) {
+			this.hold_off();	// clear hold before new suggestion
+			document.location = document.location;	// refresh suggestion
 		}
 	});
 };
